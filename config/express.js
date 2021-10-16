20c40279a86e3470b2f9fc89200de974806981c6
haujin <hujhuj0110@gmail.com>
2021-10-17 03:26:03 +0900

    [fix] 충돌해결

--- config/express.js
+++ config/express.js
@@ -7,17 +7,7 @@ module.exports = function () {
     app.use(compression());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-<<<<<<< HEAD
     app.use(express.static(process.cwd() + '/static'));
-    
-=======
-
-    app.set("view engine", "ejs");
-    app.set("views", process.cwd() + "/views");
-
-    app.use(express.static(process.cwd() + '/static'));
-
->>>>>>> 18393e1a7f9ae8902ad50c35c7d1cb6ae23e91ed
     require("../src/routes/indexRoute")(app);
 
     return app;
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,15 +1,7 @@
 module.exports = function(app) {
     const index = require("../controllers/indexController");
 
-<<<<<<< HEAD
     app.get('/', index.main);
 
     app.get('/class', index.class);
-=======
-    app.get('/', index.login);
-
-    app.post('/loginProcess', index.loginProcess);
-
-    app.get('/main', index.main);
->>>>>>> 18393e1a7f9ae8902ad50c35c7d1cb6ae23e91ed
 }
