50b85a35c510f1ee413f3c51e9a1450221527ad8
jeongsoyeon20 <jeongsoyeon0130@naver.com>
2021-10-26 06:15:58 +0900

    [feat] 분반별 상세페이지 완성

--- src/dao/indexDao.js
+++ src/dao/indexDao.js
@@ -21,6 +21,7 @@ async function checkVisitor(userMajor, userNum, userName){
 		return false;
     }
 }
+
 async function getProjects(selectClass){
     try{
         const connection = await pool.getConnection(async (conn)=> conn);
@@ -42,14 +43,34 @@ async function getProjects(selectClass){
 		return false;
     }
 }
-
-async function classTeam(project_id){
+async function getHashtags(project_id){
     const connection = await pool.getConnection(async (conn)=> conn);
-    const Query=`SELECT student_name FROM member WHERE project_id=?;`;
-    const Params=[project_id];
+    const Query=`select hashtag_name from hashtag INNER join hashtag_team on hashtag.hashtag_id=hashtag_team.hashtag_id where project_id=?;`;
+    const Params =[project_id];
     const [rows] = await connection.query(Query, Params);
     connection.release();
     return [rows];
+}
+
+async function classTeam(project_id){
+    try{
+        const connection = await pool.getConnection(async (conn)=> conn);
+        try{
+            const Query=`SELECT student_name FROM member WHERE project_id=?;`;
+            const Params=[project_id];
+            const [rows] = await connection.query(Query, Params);
+            connection.release();
+            return [rows];
+        }catch(err){
+            console.log('Query Error',err);
+            connection.release();
+            return false;
+        }
+    }
+    catch(err){
+        console.log('DB Error');
+		return false;
+    }
 }
 async function addTeam(name){
     console.log(name);
@@ -58,6 +79,7 @@ async function addTeam(name){
 module.exports = {
     checkVisitor,
     getProjects,
+    getHashtags,
     classTeam,
     addTeam
 }
