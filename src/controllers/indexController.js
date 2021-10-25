455e5f8247e40bad5f4f35fcfaebc1aed15a0aaf
jeongsoyeon20 <jeongsoyeon0130@naver.com>
2021-10-25 13:11:50 +0900

    [feat] rds 연결

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,4 @@
 const indexDao = require("../dao/indexDao");
-const db=require('../../config/database');
 
 exports.login = async function(req, res){
     console.log('who');
@@ -8,22 +7,21 @@ exports.login = async function(req, res){
 
 exports.loginProcess = async function(req, res){
     var userData=req.body;
-    console.log(userData);
-    if(userData.userName.length<2 || userData.studentId.length<2 || userData.department.length<1){
-        var data={status:400};//대충 지정함
-        return res.send(data);
-    }
-    const query= 'INSERT INTO user (user_id, user_major, user_num, user_name, login_time) VALUES (?,?,?,?,?)';
-    db.query(query,[1,userData.department,userData.studentId, userData.userName,1111],(err,result)=>{
-        if (err) throw err;
-        var data={status:200};
-        return res.send(data);
+    var userMajor = userData.department;
+    var userNum = userData.studentId;
+    var userName = userData.userName;
+    const loginResult = await indexDao.checkVisitor(userMajor, userNum, userName);
+    req.session.name = userName;
+    req.session.save(function(){
+        const data = {"status": 200};
+        res.send(data)
     });
-    
 }
 
 exports.main = async function (req, res){
-    return res.render("main.ejs");
+    const who = req.session.name;
+    console.log("login name", who);
+    return res.render("main.ejs", {who});
 }
 
 exports.class = async function(req, res){
